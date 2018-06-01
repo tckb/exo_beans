ced836d88c06b4f53d3dfc828537a0d3fe6e2119
Chandra Tungathurthi <chandra@jivox.com>
2018-06-02 01:06:32 +0530

    Added client job testcases

---
+++ test/exo_beans_job_test.exs
@@ -0,0 +1,168 @@
+defmodule ExoBeans.Test.Client do
+  use ExUnit.Case, async: true
+
+  @client_metadata Application.get_env(:exo_beans, :client_metadata)
+  @job_data_table Application.get_env(:exo_beans, :job_data_table)
+
+  alias ExoBeans.Tube.Registry
+
+  alias ExoBeans.Constants, as: Constant
+  alias Constant.Commands.Producer
+  alias Constant.Commands.Worker
+  alias Constant.Commands
+  alias ExoBeans.Client.Command.Dispatcher
+  alias ExoBeans.Constants.ClientCommands
+  alias ExoBeans.Tube.Job
+  alias ExoBeans.Server.Reply
+
+  require Constant
+  require Commands
+  require Producer
+  require Worker
+  require ClientCommands
+
+  doctest Dispatcher
+  doctest Registry
+
+  test "client ⇄ dispatcher" do
+    client_pid = new_client()
+    send_ping(client_pid)
+
+    # inform dispatcher that client is connected
+    send(client_pid, {:client_command, :connected})
+
+    # expect some metadata about the client to be stored
+    [{^client_pid, default_tube, default_watch_list}] =
+      get_client_meta(client_pid)
+
+    {^default_tube, default_tube_pid} = Registry.find_tube(default_tube)
+    # check if default tube is alive
+    assert Process.alive?(default_tube_pid) == true
+
+    # check if all the tubes in the watch list are actually alive
+    default_watch_list
+    |> MapSet.to_list()
+    |> Enum.each(fn watch_tube ->
+      {^watch_tube, watch_tube_pid} = Registry.find_tube(watch_tube)
+      assert Process.alive?(watch_tube_pid) == true
+    end)
+
+    # inform dispatcher that client is connected
+    send(client_pid, {:client_command, :disconnected})
+
+    assert get_client_meta(client_pid) == []
+  end
+
+  test "client ⇄ job" do
+    {:ok, {_, tube_pid}} = Registry.default_tube()
+
+    client_pid = new_client()
+    send_ping(client_pid)
+
+    # create a job
+    length = 1000
+    some_data = next_bytes(length)
+    delay = 0
+    job = Job.new({length, some_data}, job_opts(delay, 1, 10))
+
+    # inform dispatcher that client is connected
+    send(client_pid, {:client_command, :connected})
+    # client job save
+    send(client_pid, {:client_command, {ClientCommands.job_save(), job}})
+
+    # see if the job has been inserted
+    assert_receive {{:tube, ^tube_pid}, data}, 1_000
+
+    job_id = validate_replies(ClientCommands.job_save(), data)
+
+    # job id is always a positive integer
+    assert String.to_integer(job_id) > 0
+
+    # client asking for reservation
+    send(client_pid, {:client_command, {ClientCommands.job_request(), []}})
+
+    assert_receive {{:tube, ^tube_pid}, raw_job_data}, 1_000
+
+    job_data = validate_replies(ClientCommands.job_request(), raw_job_data)
+
+    # inform dispatcher that client is connected
+    send(client_pid, {:client_command, :disconnected})
+  end
+
+  defp job_opts(job_delay, job_priority, job_ttr) do
+    [priority: job_priority, time_to_run: job_ttr, init_delay: job_delay]
+  end
+
+  defp next_bytes(size) do
+    0..size
+    |> Enum.reduce("", fn _, acc ->
+      acc <> (0..255 |> Enum.random() |> <<>>)
+    end)
+  end
+
+  defp next_string(size) do
+    0..size
+    |> Enum.reduce("", fn _, acc ->
+      acc <> (?a..?z |> Enum.random() |> <<>>)
+    end)
+  end
+
+  defp new_client do
+    spawn(__MODULE__, :client_loop, [self()])
+  end
+
+  defp get_client_meta(client_pid) do
+    send(client_pid, {:admin_command, :dispatcher_status})
+    assert_receive client_metadata, 1_000
+
+    client_metadata
+    |> Enum.filter(fn {pid, _, _} -> pid == client_pid end)
+  end
+
+  defp dump_meta do
+    :ets.tab2list(@client_metadata)
+  end
+
+  defp send_ping(pid) do
+    # validate if the client is actually working
+    send(pid, {:ping, self()})
+    assert_receive :pong, 1_000
+  end
+
+  defp validate_replies(command, reply) do
+    server_reply = Reply.deserialize(reply)
+
+    case command do
+      ClientCommands.job_save() ->
+        assert ["INSERTED", job_id] = server_reply
+        job_id
+    end
+  end
+
+  def client_loop(calling_pid) do
+    receive do
+      {:ping, someone} ->
+        send(someone, :pong)
+
+      {:reply, from, data} ->
+        send(calling_pid, {from, data})
+
+      {:client_command, :connected} ->
+        Dispatcher.client_connected()
+
+      {:client_command, :disconnected} ->
+        Dispatcher.client_disconnected()
+
+      {:client_command, message} ->
+        Dispatcher.dispatch_command(:client, message)
+
+      {:admin_command, :dispatcher_status} ->
+        Dispatcher.dispatch_command(:admin, {:status, []})
+
+      data ->
+        send(calling_pid, data)
+    end
+
+    client_loop(calling_pid)
+  end
+end
