a11f15251dbcbadd9c7c6a5bb7843582940c6379
Chandra Tungathurthi <chandra@jivox.com>
2018-06-04 15:41:04 +0530

    update blackbox testcase

--- test/exo_beans_blackbox_test.exs
+++ test/exo_beans_blackbox_test.exs
@@ -1,5 +1,5 @@
 defmodule ExoBeans.Test.BlackBox do
-  use ExUnit.Case, async: true
+  use ExUnit.Case, async: false
   @server_port Application.get_env(:exo_beans, :port)
   alias ExoBeans.Constants.ClientCommands
   require ClientCommands
@@ -25,17 +25,136 @@ defmodule ExoBeans.Test.BlackBox do
       :ok = client |> :ebeanstalkd.close()
     end
 
-    test "single delayed job :: put, reserve, delete" do
+    test "single delayed job :: put" do
+      {:ok, client} = :ebeanstalkd.connect([{:port, @server_port}])
+
+      len = :rand.uniform(100)
+      job_data = next_bytes(len)
+      job_delay_secs = 3
+
+      {:inserted, job_id} =
+        client
+        |> :ebeanstalkd.put(job_data, [
+          {:pri, 0},
+          {:delay, job_delay_secs},
+          {:ttr, 100}
+        ])
+
+      :timer.sleep(job_delay_secs * 1000)
+      {:reserved, ^job_id, ^job_data} = client |> :ebeanstalkd.reserve()
+
+      :ok = client |> :ebeanstalkd.close()
+    end
+
+    test "multiple jobs :: put with priority" do
       {:ok, client} = :ebeanstalkd.connect([{:port, @server_port}])
 
       len = :rand.uniform(100)
       job_data = next_bytes(len)
 
-      {:inserted, job_id} =
+      {:inserted, first_pri_job} =
         client
-        |> :ebeanstalkd.put(job_data, [{:pri, 0}, {:delay, 10}, {:ttr, 60}])
+        |> :ebeanstalkd.put(job_data, [
+          {:pri, 0},
+          {:delay, 0},
+          {:ttr, 100}
+        ])
 
-      {:reserved, ^job_id, ^job_data} = client |> :ebeanstalkd.reserve()
+      {:inserted, third_pri_job} =
+        client
+        |> :ebeanstalkd.put(job_data, [
+          {:pri, 10},
+          {:delay, 0},
+          {:ttr, 100}
+        ])
+
+      {:inserted, second_pri_job} =
+        client
+        |> :ebeanstalkd.put(job_data, [
+          {:pri, 5},
+          {:delay, 0},
+          {:ttr, 100}
+        ])
+
+      {:reserved, ^first_pri_job, ^job_data} = client |> :ebeanstalkd.reserve()
+      {:deleted} = client |> :ebeanstalkd.delete(first_pri_job)
+
+      {:reserved, ^second_pri_job, ^job_data} = client |> :ebeanstalkd.reserve()
+      {:deleted} = client |> :ebeanstalkd.delete(second_pri_job)
+
+      {:reserved, ^third_pri_job, ^job_data} = client |> :ebeanstalkd.reserve()
+      {:deleted} = client |> :ebeanstalkd.delete(third_pri_job)
+
+      :ok = client |> :ebeanstalkd.close()
+    end
+
+    test "multiple job ::  put, delete & reserve" do
+      {:ok, client} = :ebeanstalkd.connect([{:port, @server_port}])
+
+      len = :rand.uniform(100)
+      job_data = next_bytes(len)
+
+      {:inserted, first_pri_job} =
+        client
+        |> :ebeanstalkd.put(job_data, [
+          {:pri, 0},
+          {:delay, 0},
+          {:ttr, 100}
+        ])
+
+      {:inserted, third_pri_job} =
+        client
+        |> :ebeanstalkd.put(job_data, [
+          {:pri, 10},
+          {:delay, 0},
+          {:ttr, 100}
+        ])
+
+      {:inserted, second_pri_job} =
+        client
+        |> :ebeanstalkd.put(job_data, [
+          {:pri, 5},
+          {:delay, 0},
+          {:ttr, 100}
+        ])
+
+      {:reserved, ^first_pri_job, ^job_data} = client |> :ebeanstalkd.reserve()
+
+      # now, delete the other jobs
+      {:deleted} = client |> :ebeanstalkd.delete(second_pri_job)
+      {:deleted} = client |> :ebeanstalkd.delete(third_pri_job)
+      {:deleted} = client |> :ebeanstalkd.delete(first_pri_job)
+
+      :ok = client |> :ebeanstalkd.close()
+    end
+
+    test "single job :: put, reserve with ttr expiry" do
+      {:ok, client} = :ebeanstalkd.connect([{:port, @server_port}])
+
+      len = :rand.uniform(100)
+      job_data = next_bytes(len)
+      job_time_to_run = 5
+
+      {:inserted, job} =
+        client
+        |> :ebeanstalkd.put(job_data, [
+          {:pri, 0},
+          {:delay, 0},
+          {:ttr, job_time_to_run}
+        ])
+
+      {:reserved, ^job, ^job_data} = client |> :ebeanstalkd.reserve()
+
+      # block for more jobs, after `job_time_to_run-1` server will inform that deadline for previous job is soon approching!
+      {:deadline_soon} = client |> :ebeanstalkd.reserve()
+
+      # do nothing! do not release the job, server will automatically release is
+      # sleep, have a coffee or whatever
+      :timer.sleep(2_000)
+
+      # ask for reservation again, it will give the same job
+      {:reserved, ^job, ^job_data} = client |> :ebeanstalkd.reserve()
+      {:deleted} = client |> :ebeanstalkd.delete(job)
 
       :ok = client |> :ebeanstalkd.close()
     end
